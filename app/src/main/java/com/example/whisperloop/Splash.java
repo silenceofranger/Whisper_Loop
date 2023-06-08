050d358149bde48f7254776bae34a3ca3095a3cf
dikshusharma <hatkotidikshu@gmail.com>
2023-06-08 11:14:27 +0530

    SignIn & SignUp Pages

--- app/src/main/java/com/example/whisperloop/Splash.java
+++ app/src/main/java/com/example/whisperloop/Splash.java
@@ -14,7 +14,7 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(Splash.this, MainActivity.class);
+            Intent intent = new Intent(Splash.this, SignIn.class);
             startActivity(intent);
             finish();
         }, 3000);
