09bdb87d7ec051131a62da193e5581bf5ed2809e
DIKSHU SHARMA <dikshusharma@ranger.local>
2023-06-08 10:08:02 +0530

    splash screen

---
+++ app/src/main/java/com/example/whisperloop/AuthenticationActivity.java
@@ -0,0 +1,13 @@
+package com.example.whisperloop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
